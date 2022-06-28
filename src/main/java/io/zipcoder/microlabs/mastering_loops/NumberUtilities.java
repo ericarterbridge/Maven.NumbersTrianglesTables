6c18a2149fa9ea0ac5b3f668fe9de41a0041650a
Eric <eric@zipcoders-mbp-6.myfiosgateway.com>
2022-06-27 21:33:38 -0400

    all classes completed

--- src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
+++ src/main/java/io/zipcoder/microlabs/mastering_loops/NumberUtilities.java
@@ -3,34 +3,66 @@ package io.zipcoder.microlabs.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        StringBuilder myString = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if (i%2 == 0) {
+                myString.append(i);
+            }
+        }
+        return myString.toString();
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        StringBuilder myString = new StringBuilder();
+        for (int i = start; i < stop; i++) {
+            if (i%2 != 0) {
+                myString.append(i);
+            }
+        }
+        return myString.toString();
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        StringBuilder squares = new StringBuilder();
+        for (int i = start; i<stop; i += step){
+            squares.append(i*i);
+        }
+        return squares.toString();
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String myString = "";
+        for (int i = 0; i < stop; i++) {
+            myString += Integer.toString(i);
+        }
+        return myString;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String myString = "";
+        for (int i = start; i < stop; i++) {
+            myString += Integer.toString(i);
+        }
+        return myString;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        StringBuilder myString = new StringBuilder();
+        for (int i = start; i < stop; i += step) {
+            myString.append(i);
+        }
+        return myString.toString();
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        StringBuilder expo = new StringBuilder();
+        for (int i = start; i < stop; i += step) {
+            expo.append((int) Math.pow(i, exponent));
+        }
+        return expo.toString();
     }
 }
